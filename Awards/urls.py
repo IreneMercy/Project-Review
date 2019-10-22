81eb8390806a62295349c356cbce993df6bf89de
IreneMercy <irenemercy700@gmail.com>
2019-10-22 13:26:27 +0300

    add api urls

--- Awards/urls.py
+++ Awards/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('register/', views.registration, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('new-project/', views.postproject, name='newproject'),
     path('project/<id>', views.get_project, name='project'),
