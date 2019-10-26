62961f2f0e371dead442957c23dfb177d9f17a7f
IreneMercy <irenemercy700@gmail.com>
2019-10-26 14:22:08 +0300

    fix bugs

--- Awards/urls.py
+++ Awards/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('updateprofile/', views.updateprofile, name='updateprofile'),
     path('new-project/', views.postproject, name='newproject'),
     path('project/<id>', views.get_project, name='project'),
     path('search', views.search_projects, name='search'),
